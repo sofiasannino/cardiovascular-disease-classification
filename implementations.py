af06c593197f5748941112f24723bc6e9ad8e91d
sofiasannino <sofia.sannino@gmail.com>
2025-09-25 14:31:45 +0200

    chanhges added

--- implementations.py
+++ implementations.py
@@ -71,8 +71,13 @@ def compute_gradient(y, tx, w):
     return (-1 / N) * tx.T @ e
 
 def sigmoid(z):
-    """Numerically stable sigmoid."""
-    return 1 / (1 + np.exp(-z))
+    """Numerically stable sigmoid (avoiding overflow)."""
+    result = np.empty_like(z, dtype=np.float64)
+    pos_mask = z >= 0
+    neg_mask = ~pos_mask
+    result[pos_mask] = 1 / (1 + np.exp(-z[pos_mask]))
+    result[neg_mask] = np.exp(z[neg_mask]) / (1 + np.exp(z[neg_mask]))
+    return result
 
 
 def compute_logistic_gradient(y, tx, w):
@@ -80,7 +85,7 @@ def compute_logistic_gradient(y, tx, w):
 
     Args:
         y: shape=(N, )
-        tx: shape=(N,2)
+        tx: shape=(N,d)
         w: shape=(d, ). The vector of model parameters.
 
     Returns:
@@ -103,31 +108,29 @@ def compute_logistic_gradient(y, tx, w):
 
 
 ### OPTIMIZATION ALGORITHMS ###
-def logistic_gradient_descent(y, tx, initial_w, max_iters, gamma):
-    """The Gradient Descent (GD) algorithm applied to logistic regression.
-
-    Args:
-        y: shape=(N, )
-        tx: shape=(N,d)
-        initial_w: shape=(d, ). The initial guess (or the initialization) for the model parameters
-        max_iters: a scalar denoting the total number of iterations of GD
-        gamma: a scalar denoting the stepsize
-
-    Returns:
-        w: w optimal through GD
-        loss: loss function value evaluated at w optimal
-        
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
     """
-    
+    Logistic regression using gradient descent (y ∈ {0, 1})
+
+    INPUTS:
+                - y = numpy  array of shape (N,) containing train outputs (0, 1)
+                - tx = numpy array of shape (N, d) containing train inputs
+                - initial_w = initial weight vector of paramters
+                - max_iters= max number of iterations allowed in gradient descendent algorithm
+                - gamma = step-size
+    OUTPUTS:
+                - w = numpy arraing containing the solution paramters
+                - loss= the loss function value corresponding to the solution parameters
+    """
     w = initial_w
     
     for n_iter in range(max_iters):
         # computing gradient and loss
         grad=compute_logistic_gradient(y, tx, w)
-        loss=compute_logistic_loss(y, tx, w)
         
         # update w by gradient
         w = w - gamma * grad
+    loss=compute_logistic_loss(y, tx, w) #compute optimal loss
 
     return w, loss
 
@@ -184,20 +187,22 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
 def ridge_regression(y, tx, lambda_):
     """
     Ridge regression using normal equations'
-    'INPUTS:
-    '        - y = numpy  array of shape (N,) containing train outputs
-    '        - tx = numpy array of shape (N, d) containing train inputs
-    '        - lambda_ = ridge regression parameter
-    'OUTPUTS:
-    '        -w = numpy arraing containing the solution paramters
-    '        -loss= the loss function value corresponding to the solution parameters, WITHOUT penalizing term
+    INPUTS:
+            - y = numpy  array of shape (N,) containing train outputs
+            - tx = numpy array of shape (N, d) containing train inputs
+            - lambda_ = ridge regression parameter
+    OUTPUTS:
+            -w = numpy arraing containing the solution paramters
+            -loss= the loss function value corresponding to the solution parameters, WITHOUT penalizing term
     """
-    # sample size
+        # sample size
     N= len(y)
 
     #optimal parameters vector
     lambda_1= lambda_ * 2 * N
-    w= (np.linalg.inv(tx.T @ tx + lambda_1 @ np.eye(tx.shape[1])))@ tx.T @ y
+    A=tx.T @ tx + lambda_1 * np.eye(tx.shape[1])
+    b=tx.T @ y
+    w= np.linalg.solve(A, b)
 
     #loss L(w) without penalizing term
     loss = compute_mse_loss(y, tx, w)
@@ -207,24 +212,7 @@ def ridge_regression(y, tx, lambda_):
 
 
 
-def logistic_regression(y, tx, initial_w, max_iters, gamma):
-    """
-    Logistic regression using gradient descent (y ∈ {0, 1})
 
-    INPUTS:
-                - y = numpy  array of shape (N,) containing train outputs (0, 1)
-                - tx = numpy array of shape (N, d) containing train inputs
-                - initial_w = initial weight vector of paramters
-                - max_iters= max number of iterations allowed in gradient descendent algorithm
-                - gamma = step-size
-    OUTPUTS:
-                - w = numpy arraing containing the solution paramters
-                - loss= the loss function value corresponding to the solution parameters
-    """
-
-    w, loss = logistic_gradient_descent(y, tx, initial_w, max_iters, gamma) #w optimal through logistic gradient descendent
-
-    return w, loss
 
 
 
